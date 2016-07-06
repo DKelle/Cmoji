6edf89c80f5279695770b4e26093d9710f93bc8d
Dallas Kelle <dkelle@Dallass-MacBook-Pro-2.local>
2016-07-06 12:09:27 -0400

    Changed the preprocessor to be able to correctly parse even when there aren't spaces between character

--- compiler/preprocessor.java
+++ compiler/preprocessor.java
@@ -6,75 +6,98 @@ import java.nio.charset.*;
 public class preprocessor
 {
     private HashMap<String, String> unicodeToKey;
-    private boolean debug = false;
+    private boolean debug = true;
 
     public static void main(String[] args)
     {
-	new preprocessor();
+	    new preprocessor();
     }
 
     public preprocessor()
     {
-	initHash();
+	    initHash();
         InputStreamReader isReader = new InputStreamReader(System.in);
         BufferedReader bufReader = new BufferedReader(isReader);
-	ArrayList<String> outfile = new ArrayList<String>();
-	boolean done = false;
-	byte[] utf8Bytes;
-	int varnum = 0;	
+	    ArrayList<String> outfile = new ArrayList<String>();
+	    boolean done = false;
+	    byte[] utf8Bytes;
+	    int varnum = 0;	
+        String emoji = "";
 
         while(!done){
             try {
                 String inputStr;
-                if((inputStr=bufReader.readLine()) != null) {
-		    for(String emoji : inputStr.split(" "))
-		    {
-			if(!unicodeToKey.containsKey(escapeUnicode(emoji)))
-			{
-			    unicodeToKey.put(escapeUnicode(emoji), "var"+varnum);
-			    varnum++;
-			}
-		        outfile.add(unicodeToKey.get(escapeUnicode(emoji)));
-		    }
-		    outfile.add("\n");
-		}
-                else {
-		    done = true;
+                if((inputStr=bufReader.readLine()) != null) 
+                {
+                    inputStr = inputStr.replaceAll("\\s+", "");
+                    String escapedStr = escapeUnicode(inputStr);
+                    int i = 0;
+                    int substringLen = 5;
+                    String numbers = "0123456789";
+                    while(i < escapedStr.length() - 6)
+                    {
+                        //Each emoji is 2 sets of 5 chars long Ex: \\ud83d\\udda
+                        //Except numbers, which has 6 chars, and then 5 chars Ex: 5\ufe0f\u20e3
+ 
+                        //Build up the first part of the emoji (which may have the extra char)
+                        if(numbers.contains(escapedStr.substring(i,i+1)))
+                        {
+                            emoji += escapedStr.substring(i,i+7);
+                            i += 7;
+                        }
+                        else
+                        {
+                            emoji += escapedStr.substring(i,i+6);
+                            i += 6;
+                        }
+
+                        //build up the second part of the emoji (which will never have the extra char)
+                        emoji += escapedStr.substring(i,i+6);
+                        i += 6;
+
+                        if(!unicodeToKey.containsKey(escapeUnicode(emoji)))
+                        {
+                            unicodeToKey.put(escapeUnicode(emoji), "var"+varnum);
+                            varnum++;
+                        }
+                        outfile.add(unicodeToKey.get(escapeUnicode(emoji)));
+                        emoji = "";
+                    }
+                    outfile.add("\n");
+                    
+		        }
+                else 
+                {
+		            done = true;
                 }
             }
-            catch (Exception e) {
-	    	e.printStackTrace();
-	    }
+            catch (Exception e) 
+            {
+	    	    e.printStackTrace();
+	        }
         }
 
-	if(debug)
-	{
-	    for(String str : outfile)
-	    {
-		System.out.println(str);
-	    }
-	}
-
-	createOutput(outfile);
+	    createOutput(outfile);
 
     }
 
     public void createOutput(ArrayList<String> outfile)
     {
-	FileWriter writer = null;
-	try
-	{
-	    writer = new FileWriter("preprocessed.txt"); 
-	    for(String str: outfile) {
- 		 writer.write(str+" ");
+	    FileWriter writer = null;
+	    try
+	    {
+	        writer = new FileWriter("preprocessed.txt"); 
+	        for(String str: outfile)  
+            {
+ 		        writer.write(str+" ");
+	        }
+	        writer.close();
 	    }
-	    writer.close();
-	}
-	catch(Exception e)
-	{
-	    System.out.println("Could not create preprocessed.txt");
-	    e.printStackTrace();
-	}
+	    catch(Exception e)
+	    {
+	        System.out.println("Could not create preprocessed.txt");
+	        e.printStackTrace();
+	    }
     }
 
     public String escapeUnicode(String input)
@@ -97,37 +120,36 @@ public class preprocessor
 
     public void initHash()
     {
-
-	unicodeToKey = new HashMap<String, String>();
+	    unicodeToKey = new HashMap<String, String>();
 
         String fileName = "initmap.txt";
         String line = null;
 
-        try {
+        try 
+        {
             FileReader fileReader = new FileReader(fileName);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-            while((line = bufferedReader.readLine()) != null) {
-		line = line.replaceAll("\\s+", "");
-		String[] lines = line.split("//");
-		if(lines.length > 2)
-		{
-		    System.out.println("ERROR: emoji unicode contains '//'");
-		    System.exit(0);
-		}
+            while((line = bufferedReader.readLine()) != null) 
+            {
+		        line = line.replaceAll("\\s+", "");
+		        String[] lines = line.split("//");
+		        if(lines.length > 2)
+		        {
+		            System.out.println("ERROR: emoji unicode contains '//'");
+		            System.exit(0);
+		        }
 		
-		if(debug) for(String s : lines) System.out.println(s);
-
-		unicodeToKey.put(escapeUnicode(lines[0]), lines[1]);
+		        unicodeToKey.put(escapeUnicode(lines[0]), lines[1]);
                 //System.out.println(line);
             }
 
             bufferedReader.close();
         }
-        catch(IOException e) {
+        catch(IOException e) 
+        {
             e.printStackTrace();
         }
-
     }
 
 }
